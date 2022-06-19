ea934215b03f84ab4fc1b0d8c7fe3f7a619f8e81
Cybergoat9152 <arthurtbarros@outlook.com>
2022-06-19 19:43:51 -0300

    feat: creating login component

--- hammerspace-app/src/app/app.module.ts
+++ hammerspace-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppLoginComponent } from './app-login/app-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLoginComponent
   ],
   imports: [
     BrowserModule,
