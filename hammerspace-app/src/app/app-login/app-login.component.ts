5db4ab10e0dc6fed85e353988cd8288624415c5c
Cybergoat9152 <arthurtbarros@outlook.com>
2022-06-19 23:29:47 -0300

    feat: adding login logic int frontend

--- hammerspace-app/src/app/app-login/app-login.component.ts
+++ hammerspace-app/src/app/app-login/app-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-app-login',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppLoginComponent implements OnInit {
 
-  constructor() { }
+  formGroup!: FormGroup;
 
+  constructor(private authService: AuthService) { }
   ngOnInit(): void {
+    this.initForm();
   }
 
+  initForm(){
+    this.formGroup = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    })
+  }
+  LoginProcess(){
+    if(this.formGroup.valid){
+      this.authService.login(this.formGroup.value).subscribe((result) => {
+        if(result.success){
+          console.log(result.message);
+          alert(result.message);
+        } else {
+          console.log(result.message);
+          alert(result.message);
+        }
+      })
+    }
+  }
 }
