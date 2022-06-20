da56b464caf6d8239fc09a6bf21757b93f02f8e5
Cybergoat9152 <arthurtbarros@outlook.com>
2022-06-19 23:11:31 -0300

    feat: create login service

--- hammerspace-app/src/app/auth.service.ts
+++ hammerspace-app/src/app/auth.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { httpUrl } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  login(data: any):Observable<any>{
+    return this.http.post(`${httpUrl}/auth`, data);
+  }
 }
