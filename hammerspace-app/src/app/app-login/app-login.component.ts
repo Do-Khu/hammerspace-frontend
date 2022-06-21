d6c7a62d3ffe900148846254cc1d18d27f52bb64
Cybergoat9152 <arthurtbarros@outlook.com>
2022-06-20 21:16:13 -0300

    feat: adding hash feat to password

--- hammerspace-app/src/app/app-login/app-login.component.ts
+++ hammerspace-app/src/app/app-login/app-login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import * as shajs from 'sha.js';
 
 @Component({
   selector: 'app-app-login',
@@ -22,14 +23,22 @@ export class AppLoginComponent implements OnInit {
       password: new FormControl('', [Validators.required]),
     })
   }
+
+  hashPassword(pass: string) {
+    this.formGroup.value.password = shajs('sha256').update(pass).digest('hex');
+  }
+
   LoginProcess(){
     if(this.formGroup.valid){
+      this.hashPassword(this.formGroup.value.password);
       this.authService.login(this.formGroup.value).subscribe(Response => {
         if(Response.token != undefined){
           alert("logado com sucesso\n"+Response.token);
         } else {
-          alert("falha no login");
+          alert("Wrong Login or password");
         }
+      }, err => {
+        alert("Login fail");
       })
     }
   }
