eebba0b2e7b7001f71a43a9d77d98a1e7a04caa1
cyebrgoat9152 <arthurtbarros@outlook.com>
2022-10-16 21:12:47 -0300

    cleaning previous project

--- hammerspace-app/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true
-};
-
-const PORT = 5000;
-const HOST_DNS = 'localhost';
-const ROOT_PATH = 'api';
-
-export const httpUrl = `http://${HOST_DNS}:${PORT}/${ROOT_PATH}`;
--- hammerspace-app/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false
-};
-
-const PORT = 9152;
-const HOST_DNS = 'localhost';
-const ROOT_PATH = 'api';
-
-export const httpUrl = `http://${HOST_DNS}:${PORT}/${ROOT_PATH}`;
